36449fb089d8ae73e0f3065080fe9212587702fe
Thomas Broussoux <thbrouss@z1r3p2.le-101.fr>
2019-05-28 23:52:52 +0200

    matcha setup

---
+++ public/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello {{name}}</h1>
+    <div>
+      <h1>Hello {{name}}</h1>
+    </div>
+    <div>
+      <h1>Hello {{name}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            props: ['name']
+        }
+    }
+</script>
